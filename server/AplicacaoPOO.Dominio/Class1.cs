447393f4ed50a7f31c6e2d70892737574fa0109b
Yuri Costa e Silva Eduardo <yuri.tag145@gmail.com>
2022-09-12 21:56:37 -0300

    #1 Inicializando

---
+++ server/AplicacaoPOO.Dominio/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace AplicacaoPOO.Dominio
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ server/AplicacaoPOO.Dominio/Debito.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacaoPOO.Dominio
+{
+    public internal class Debito
+    {
+        Pagamento Pagamento { get; set; }
+        double Desconto { get; set; } 
+    }
+}
