f4803421a228fc74be302e1fafb832b101a8e7b0
Yuri Costa e Silva Eduardo <yuri.tag145@gmail.com>
2022-09-13 21:53:37 -0300

    Adicionado formulario cliente

---
+++ client/AplicacaoPoo.Windows/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace AplicacaoPoo.Windows
+{
+    partial class frmCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValorDebito = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.lblValorCredito = new System.Windows.Forms.Label();
+            this.lblValorDebito = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtValorDebito
+            // 
+            this.txtValorDebito.Location = new System.Drawing.Point(114, 170);
+            this.txtValorDebito.Name = "txtValorDebito";
+            this.txtValorDebito.Size = new System.Drawing.Size(100, 23);
+            this.txtValorDebito.TabIndex = 0;
+            this.txtValorDebito.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 225);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(114, 50);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 23);
+            this.txtNome.TabIndex = 2;
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(114, 113);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(100, 23);
+            this.txtCpf.TabIndex = 3;
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(114, 95);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(28, 15);
+            this.lblCpf.TabIndex = 4;
+            this.lblCpf.Text = "CPF";
+            // 
+            // lblValorCredito
+            // 
+            this.lblValorCredito.AutoSize = true;
+            this.lblValorCredito.Location = new System.Drawing.Point(114, 207);
+            this.lblValorCredito.Name = "lblValorCredito";
+            this.lblValorCredito.Size = new System.Drawing.Size(92, 15);
+            this.lblValorCredito.TabIndex = 5;
+            this.lblValorCredito.Text = "VALOR CREDITO";
+            // 
+            // lblValorDebito
+            // 
+            this.lblValorDebito.AutoSize = true;
+            this.lblValorDebito.Location = new System.Drawing.Point(114, 152);
+            this.lblValorDebito.Name = "lblValorDebito";
+            this.lblValorDebito.Size = new System.Drawing.Size(84, 15);
+            this.lblValorDebito.TabIndex = 6;
+            this.lblValorDebito.Text = "VALOR DEBITO";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(114, 32);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(42, 15);
+            this.lblNome.TabIndex = 7;
+            this.lblNome.Text = "NOME";
+            // 
+            // frmCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 338);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblValorDebito);
+            this.Controls.Add(this.lblValorCredito);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtValorDebito);
+            this.Name = "frmCliente";
+            this.Text = "Cliente ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtValorDebito;
+        private TextBox textBox2;
+        private TextBox txtNome;
+        private TextBox txtCpf;
+        private Label lblCpf;
+        private Label lblValorCredito;
+        private Label lblValorDebito;
+        private Label lblNome;
+    }
+}
